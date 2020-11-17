70674791c8f038f2a9a54281ed91dffa41bc24ee
JiWon Lee <jiwon.swdev@gmail.com>
2020-11-17 23:14:07 +0900

    feat: 추가 과제
    - 글꼴 크기 변경
    - 이미지 이름순으로 정렬
    - Detail 화면 이름 변경

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -28,7 +28,8 @@ class ViewController: UITableViewController {
             }
         }
         
-        print(pictures)
+        // *: 이미지 이름을 기준으로 정렬
+        pictures.sort()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -46,6 +47,11 @@ class ViewController: UITableViewController {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             // 2: 성공! selectedImage 프로퍼티 설정
             vc.selectedImage = pictures[indexPath.row]
+            
+            // *: 이름 지정
+            // Picture X of Y, Y is total number of images and X is selected picture's position in the array.
+            // Make sure you count from 1 rather than
+            vc.title = "Picture \(indexPath.row + 1) of \(pictures.count)"
             
             // 3: navigation controller로 push
             navigationController?.pushViewController(vc, animated: true)
