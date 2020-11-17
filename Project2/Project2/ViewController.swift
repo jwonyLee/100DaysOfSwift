ea68a136c1acfca290030c9784f73c6bb4ab9370
JiWon Lee <jiwon.swdev@gmail.com>
2020-11-17 23:50:18 +0900

    feat: 게임 기본 작업

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    var countries = [String]()
+    var score = 0
 
     // MARK: - @IBOutlet Properties
     @IBOutlet weak var button1: UIButton!
@@ -16,9 +20,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
+        button2.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
+        button3.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
     }
 
-
+    func askQuestion(action: UIAlertAction! = nil) {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 }
 
