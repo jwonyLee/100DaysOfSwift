4b4005666b0fdc314bb05769153f97b4e7d81e29
JiWon Lee <jiwon.swdev@gmail.com>
2021-04-07 20:08:29 +0900

    feat: Project5 - Setting Up

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by 이지원 on 2021/04/07.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
